ed090d022e9e1519093d354e850ec2c7247e5de9
bagas2706 <2031710108@student.polinema.ac.id>
2022-06-21 20:35:24 +0700

    update

---
+++ lib/main_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_movie_app/movie%20by%20popularity/movie_popularity_widget.dart';
+import 'package:flutter_movie_app/movie%20by%20year/movie_year_widget.dart';
+import 'package:flutterfire_ui/auth.dart';
+
+import 'movie_bucket_list/movie_bucket_list.dart';
+
+class MainPage extends StatefulWidget {
+  MainPage({Key? key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  //User? currenUser = FirebaseAuth.instance.currentUser; //get current user information
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.purple[600],
+            title: const Text('Movie App - Kelompok 5'),
+            actions: const [SignOutButton()],
+            bottom: const TabBar(tabs: [
+              Tab(child: Text('Popular')),
+              Tab(
+                child: Text('Coming Soon'),
+              ),
+              Tab(
+                child: Text('My Bucket List'),
+              )
+            ]),
+          ),
+          body: TabBarView(
+            children: [
+              const MovieYearCustomCard(),
+              const MoviePopularityCustomCard(),
+              MovieBucketListWidget(),
+            ],
+          ),
+        ));
+  }
+}
